e1fa3efe23eb5bd92796badb0a1db4d0443f7564
esmanuryilmaz-3012 <250541033@firat.edu.tr>
2025-11-10 23:12:45 +0300

    Update FizikFormul.java

--- FizikFormul.java
+++ FizikFormul.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [Esmanur YILMAZ]
+ * Ogrenci No: [250541033]
+ * Tarih: [10.11.2025]
  * Aciklama: Gorev 2 - Fizik Formulu Asistani
  *
  * Bu program temel fizik formullerini kullanarak
